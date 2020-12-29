f49fdb3768b84a55ff23e541ed8953c6d70b6c38
sztuczkap <p.sztuczka@wp.pl>
2020-12-29 20:29:33 +0100

    interfejs Predicate

--- src/pl/sztuczkap/Main.java
+++ src/pl/sztuczkap/Main.java
@@ -1,8 +1,25 @@
 package pl.sztuczkap;
 
+import java.util.function.BiPredicate;
+import java.util.function.Predicate;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        // Predykat to interfejs który przyjmuje argument typu generycznego i zwraca wartość logiczną
+        Predicate<String> fiveCharsWordPredicate = (word) -> word.length() > 5;
+        Predicate<String> aContainsPredicate = (word) -> word.contains("a");
+
+        System.out.println(fiveCharsWordPredicate.negate()
+                .and(aContainsPredicate)
+                .test("java"));
+
+        BiPredicate<String, Integer> biPredicate = (text, number) ->
+                text.contains("@") && number > 100;
+
+        System.out.println(biPredicate.test("damian@wp.pl", 124));
+
+
     }
 }
